36395e2d1e4a2b05c31dc971e27125ff11091986
jiwon824 <59115860+jiwon824@users.noreply.github.com>
2020-01-15 23:40:53 +0900

    Add files via upload

---
+++ Java_Study_3day.java
@@ -0,0 +1,6 @@
+public class HelloWorldApp{
+	public static void main(String[] args) {
+		System.out.println("Hello world!!".length());
+		System.out.println("Hello, [[[name]]] ... bye.".replace("[[[name]]]", "egoing"));//egoing에 바꾸고 싶은 문자를 넣으면 된다.
+	}
+}
