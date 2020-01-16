df83706558d84f20aabebec33b9b4e14c537684a
jiwon824 <59115860+jiwon824@users.noreply.github.com>
2020-01-16 21:38:50 +0900

    Add files via upload

---
+++ Java_Study_4day.java
@@ -0,0 +1,15 @@
+public class MyClass {
+
+	public static void main(String args[]) {
+
+		String name = "leezche";
+
+		System.out.println("Hello, "+name+" ... "+name+" ... egoing ... bye");
+
+
+	double VAT = 10.0;
+
+	System.out.println(VAT);
+	}
+
+}
