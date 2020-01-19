83022ad04b558d876d4c3d403a496d0bb579f10d
jiwon824 <59115860+jiwon824@users.noreply.github.com>
2020-01-19 23:50:01 +0900

    Add files via upload

---
+++ Java_Study_7day.java
@@ -0,0 +1,24 @@
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Security;
+import org.opentutorials.iot.Lighting;
+
+public class OkJavaGoInHome {
+	public static void main(String args[]) {
+		
+		String id = "JAVA APT 507";
+		//Elevator call
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);//1층에서 위 층으로 올라간다는 의미
+		
+		//Security off
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+
+		//Light on
+		Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+		hallLamp.on();
+
+		Lighting floorLamp = new Lighting(id+" / floorLamp");
+		floorLamp.on();
+	}
+}
