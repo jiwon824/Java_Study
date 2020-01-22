8ef3654f0b0d5570ec8f85fd3bad54338f2971b4
jiwon824 <59115860+jiwon824@users.noreply.github.com>
2020-01-22 20:50:08 +0900

    Add files via upload

---
+++ Java_Study_10day.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class MyClass {
+	public static void main(String args[]) throws IOException {
+		System.out.println("정수를 입력하십시오.");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		int res = Integer.parseInt(str);
+
+		switch(res){
+			case 1:
+				System.out.println("1이 입력되었습니다.");
+
+			case 2:
+				System.out.println("2가 입력되었습니다.");
+			default:
+				System.out.println("1 혹은 2를 입력하십시오.");
+				break;
+	}
+}
